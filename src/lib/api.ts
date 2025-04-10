3a402b93e1dee7675a6c2e68f8936885d6d92688
bomjan133 <bomjan133@gmail.com>
2025-04-10 13:57:30 +0545

    toast info and navigate

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -13,12 +16,15 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && (error.response.status === 401 || error.response.status === 403)) {
       localStorage.removeItem('token');
-      window.location.href = '/login';
+      const navigate = useNavigate();
+      navigate('/login');
+      toast.info("You are logged out. Please log in again.");
     }
     return Promise.reject(error);
   }
